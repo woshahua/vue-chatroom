ff1981b9d2849d01ebf26994b02628b90b86b06b
woshahua <wohangye@gmail.com>
2019-06-24 18:27:20 +0900

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!--use absolute to locate in global-->
+    <v-bottom-nav absolute>
+      <v-tabs color="grey lighten-1"
+      dark 
+      fixed-tabs
+      icons-and-text
+      slider-color="yellow">
+      <v-tab>
+      Home
+      <v-icon>home</v-icon>
+      </v-tab>
+      <v-tab>
+      Robot
+      <v-icon>android</v-icon>
+      </v-tab>
+      <v-tab>
+      Account
+      <v-icon>account_circle</v-icon>
+      </v-tab>
+      </v-tabs>
+    </v-bottom-nav>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
